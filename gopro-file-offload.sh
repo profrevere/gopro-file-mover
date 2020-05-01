ae6ddb75d0f6938243d9f4b8693b8692328f70ce
profrevere <brkaminski@gmail.com>
2020-04-30 20:44:34 -0400

    streamlined script

--- gopro-file-offload.sh
+++ gopro-file-offload.sh
@@ -10,31 +10,21 @@ echo -e "What type of files are we transfering? (e.g. Videos or Pictures)"
 read fileType
 echo "Confirming that only $fileType's will be transfered."
 
-#Directory Creation
-if [ $fileType = "Videos" ]
-then
-	mkdir -p ~/Videos/$activity/$currentDate/
-fi
-
-if [ $fileType = "Pictures" ]
-then
-	mkdir -p ~/$fileType/$activity/$currentDate/
-fi
-
 #Pre-defined variables
 SOURCEDIR=/media/$USER/GoPro7/DCIM/*GOPRO/
 VIDDESTDIR=~/$fileType/$activity/$currentDate/
 PICDESTDIR=~/$fileType/$activity/$currentDate/
 
-
-#If/then statements for routing files
+#Directory Creation
 if [ $fileType = "Videos" ]
 then
+	mkdir -p ~/Videos/$activity/$currentDate/
 	rsync -avhtP --exclude-from="gopro-file-offload-Videos-exclude-list.txt" $SOURCEDIR $VIDDESTDIR | tee $VIDDESTDIR/Rsync-Automation-Transfer-Log-$activity-$currentDate.txt
 fi
 
 if [ $fileType = "Pictures" ]
 then
+	mkdir -p ~/$fileType/$activity/$currentDate/
 	rsync -avhtP --exclude-from="gopro-file-offload-Pictures-exclude-list.txt" $SOURCEDIR $PICDESTDIR | tee $PICDESTDIR/Rsync-Automation-Transfer-Log-$activity-$currentDate.txt
 fi
 
